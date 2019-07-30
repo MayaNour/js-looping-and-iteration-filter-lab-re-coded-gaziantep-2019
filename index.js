fc02a1bd6334ae44601fb256c3f7f2949e5ca408
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-30 13:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers,name){
   const matchingDrivers = drivers.filter(function (driver) {
-    return driver.toLowerCase() === name; });
+    return driver.toLowerCase() === name.toLowerCase(); });
  
  return matchingDrivers;
 }
